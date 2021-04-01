d2a3443021830a32d65dd412bf108a8ebc3e9d19
Bruno Sgotti <brunojsgotti@hotmail.com>
2021-04-01 17:25:41 -0300

    first commit

---
+++ src/components/Task.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+export default props => {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.checkContainer}>
+                {getCheckView(props.doneAt)}
+            </View>
+            <View>
+                <Text>{props.desc}</Text>
+                <Text>{props.estimateAt + ""}</Text>
+            </View>
+        </View>
+    )
+}
+
+const getCheckView = (doneAt) =>  {
+    if(doneAt != null){
+        return (
+            <View style={styles.done}>
+                <Icon name='check' size={20} color='#FFF'/>
+            </View>
+        )
+    } else {
+        return (
+            <View style={styles.pending}></View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        borderColor: '#AAA',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        paddingVertical: 10 
+    },
+    checkContainer: {
+        width: '20%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    pending: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        borderWidth: 1,
+        borderColor: '#555'
+    },
+    done: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        backgroundColor: '#4d7031',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
